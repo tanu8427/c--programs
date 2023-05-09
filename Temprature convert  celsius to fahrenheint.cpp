b3cb3a746fa260297559c8ac5300cb6b8ba4af23
Tanu kumar <132069148+tanu8427@users.noreply.github.com>
2023-05-09 20:04:40 +0530

    Create  Temprature convert Celsius to fahrenheint.cpp.

---
+++ Temprature convert  celsius to fahrenheint.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    float celsius ;
+    float tem;
+    
+    cout<<"Enter the Tempratue in celsius:";
+    cin>>celsius;
+    
+    tem =(celsius*9/5)+32;
+    cout<<"The Tempratue in fahreneit: "<<tem<<endl;
+    
+}
