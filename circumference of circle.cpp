5fc7c6bf869e5a0ef0136e89b8fdac1434cec281
Tanu kumar <132069148+tanu8427@users.noreply.github.com>
2023-05-09 19:52:15 +0530

    Create circumference of circle.cpp

---
+++ circumference of circle.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int raduis;
+    float circumference ;
+    cout<<"Enter the raduis od circle:";
+    cin>>raduis;
+    
+    circumference = 2*3.14*raduis;
+    
+    cout<< "The circumference of cicrle: "<<circumference <<endl;
+}
