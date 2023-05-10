a83a41304fb7402f4b89ee0b1f17604295419310
Tanu kumar <132069148+tanu8427@users.noreply.github.com>
2023-05-10 19:19:01 +0530

    Create Check given number is Even or Odd.cpp

---
+++ Check number is Even or Odd.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int num;
+    
+    cout<<"Enter the number:";
+    cin>>num;
+    
+    if(num%2==0){
+        cout<<"The given number is Even.";
+    }
+    
+    else{
+    cout<<"The given number is Odd.";
+    }
+
+}    
