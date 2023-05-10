7341ecd8250e2118c00f1913f7e549d336eb5b80
Tanu kumar <132069148+tanu8427@users.noreply.github.com>
2023-05-10 19:04:15 +0530

    Create  Check the two number given by user is Equal or not Equal.cpp

---
+++ Check the two number given by user is Equal or not Equal.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int a,b;
+    
+    cout<<"Enter any two number:";
+    cin>>a>>b;
+    if(a==b){
+        cout<<"The given number is equal.";
+    }
+    else{
+        cout<<"The given number is not equal:";
+    }
+}
