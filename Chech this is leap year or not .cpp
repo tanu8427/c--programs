c639ebbc26e1a3e477c6a3eee3be19cfb911b961
Tanu kumar <132069148+tanu8427@users.noreply.github.com>
2023-05-10 19:26:37 +0530

    Create Check this is leap year or not.cpp

---
+++ Chech this is leap year or not .cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+     
+     int year;
+    
+    cout<<"Enter the year:";
+    cin>>year;
+    
+    if(year%4==0 || year%100==0 || year%400==0){
+        cout<<"This is leap year.";
+    }
+    else{
+        cout<<"The is not leap year.";
+    }
+    
+}
