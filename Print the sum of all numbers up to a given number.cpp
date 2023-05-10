c91b2dca02a9cf15032533d416dc85faca0cfaf5
Tanu kumar <132069148+tanu8427@users.noreply.github.com>
2023-05-10 20:03:54 +0530

    Create Print the sum of all numbers up to a given number.cpp

---
+++ Print the sum of all numbers up to a given number.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int num;
+    int sum=0;
+    int ar[num];
+    cout<<"Enter the range upto:";
+    cin>>num;
+    
+    
+    
+    for(int i=1;i<=num;i++){
+        sum = sum+ar[i];
+    } cout<<sum;
+      cout<<endl;
+
+    return 0;
+}
