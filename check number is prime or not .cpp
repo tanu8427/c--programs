019c9a7072cce908b0c0ea5aa9d589abc6ddfd4c
Tanu kumar <132069148+tanu8427@users.noreply.github.com>
2023-05-18 01:53:01 +0530

    Create check number is prime or not.cpp

---
+++ check number is prime or not .cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int isPrime(int n){
+	if(n==2) return 1;
+	for(int i=2;i<n; i++){
+		if(n%i==0) return 0;
+	}
+	return 1;
+}
+int main(){
+	int num;
+	cin>>num;
+	cout<<(isPrime(num)?"prime":"not prime");
+}
